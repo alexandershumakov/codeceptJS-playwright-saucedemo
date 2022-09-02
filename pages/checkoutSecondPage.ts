4b13dbcfcd2ff3c1de9f5273968becd2ce390dd4
Alexander Shumakov <alexander.shumakov@gmail.com>
2022-09-02 12:52:19 +0300

    Homework for lesson 6 (cart page)

--- pages/checkoutSecondPage.ts
+++ pages/checkoutSecondPage.ts
@@ -9,6 +9,11 @@ class CheckoutSecondPage extends Page {
     private checkoutNameOfProduct: Locator = locate ("//div[contains(text(),'Sauce Labs Fleece Jacket')]").as("Product Name");
     private checkoutPriceOfProduct: Locator = locate ("//div[contains(@class,'inventory_item_price')]").as("Product Price");
     private finishButton: Locator = locate ("//button[contains(@id,'finish')]").as("Finish Button");
+    private checkoutNameOfProduct2: Locator = locate ("//div[contains(text(),'Sauce Labs Backpack')]");
+    private checkoutPriceOfProduct2: Locator = locate ("//div[contains(@class,'inventory_item_price')]");
+    private checkoutNameOfProduct3: Locator = locate ("//div[contains(text(),'Sauce Labs Bolt T-Shirt')]");
+    private checkoutPriceOfProduct3: Locator = locate ("//div[contains(@class,'inventory_item_price')]");
+
 
     constructor() {
         super("/checkout-step-two");
@@ -23,6 +28,18 @@ class CheckoutSecondPage extends Page {
     assertProduct (productData: Product) : CheckoutSecondPage {
         I.see(productData.name1, this.checkoutNameOfProduct);
         I.see(productData.cost1, this.checkoutPriceOfProduct);
+        return this;
+    }
+
+    assertProduct2 (productData: Product) : CheckoutSecondPage {
+        I.see(productData.name2, this.checkoutNameOfProduct2);
+        I.see(productData.cost2, this.checkoutPriceOfProduct2);
+        return this;
+    }
+
+    assertProduct3 (productData: Product) : CheckoutSecondPage {
+        I.see(productData.nameThirdProduct, this.checkoutNameOfProduct3);
+        I.see(productData.costThirdProduct, this.checkoutPriceOfProduct3);
         return this;
     }
 
